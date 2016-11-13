34fe62de3f68b134e14a64950ef7f74fda127207
Daniel Bershatsky <daniel.bershatsky@skolkovotech.ru>
2016-11-14 02:22:44 +0300

    Binding baseling classifier in backend API.

--- miptclass/blueprints/api.py
+++ miptclass/blueprints/api.py
@@ -5,9 +5,11 @@ import logging
 
 from datetime import datetime, timedelta
 from flask import Blueprint, current_app, jsonify, request, redirect, url_for
+from operator import itemgetter
 from requests import Session
 
 from miptclass import settings, models
+from miptclass.api import Friends
 from miptclass.blueprints import admin
 from miptclass.models import AccessToken
 
@@ -19,7 +21,10 @@ bp = Blueprint('api', __name__)
 @bp.route('/access_token', methods=['GET'])
 def index():
     if 'code' not in request.args:
-        return jsonify(error=request.args.get('error'), error_description=request.args.get('error_description')), 400
+        return (jsonify(
+                    error=request.args.get('error'),
+                    error_description=request.args.get('error_description')),
+                400)
 
     session = Session()
     response = session.get('https://oauth.vk.com/access_token', params=dict(
@@ -34,9 +39,34 @@ def index():
 
     json = response.json()
     expires_in = datetime.now() + timedelta(seconds=json['expires_in'])
-    token = AccessToken(id=json['user_id'], token=json['access_token'], expires_in=expires_in)
+    token = AccessToken(
+        id=json['user_id'],
+        token=json['access_token'],
+        expires_in=expires_in)
 
     models.db.merge(token)
     models.db.commit()
 
     return redirect(url_for('admin.index'))
+
+@bp.route('/classify/<int:uid>', methods=['GET'])
+def classify(uid):
+    token = (current_app.db.
+             query(AccessToken).
+             filter_by(id=uid).
+             scalar())
+
+    if not token:
+        return abort(404)
+
+    response = Friends(token=token.token).get(uid)
+
+    if 'error_code' in response and 'items' not in response:
+        return jsonify(response), 200
+
+    user_friends = map(itemgetter('id'), response['items'])
+    user_friends = list(user_friends)
+
+    is_mipt = current_app.classifier(user_friends)
+
+    return jsonify(is_mipt=is_mipt), 200
--- miptclass/wsgi.py
+++ miptclass/wsgi.py
@@ -3,11 +3,14 @@
 
 from miptclass import cli, blueprints, classifier
 from miptclass.app import app
+from miptclass.models import db
 
 
 blueprints.init_app(app)
 cli.init_app(app)
 classifier.init_app(app)
+
+app.db = db
 
 if app.config.get('PROFILE', False):
     from werkzeug.contrib.profiler import ProfilerMiddleware
